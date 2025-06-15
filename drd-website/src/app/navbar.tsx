bee9c3e83d6bbb7a37a95349301a9e4c410cd110
Pranav Mucharla <mucharlapranav@gmail.com>
2025-06-15 19:58:30 -0400

    Created a basic navbar

---
+++ drd-website/src/app/navbar.tsx
@@ -0,0 +1,31 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-sky-600 p-4">
+            <ul className="flex space-x-12 justify-center items-center">
+                <li className="flex items-center mr-auto">
+                    <a href="/">
+                        <img src="/logo.png" alt="Logo" className="h-8 w-auto pl-2 pr-6" />
+                    </a>
+                    <h1 className="text-3xl font-bold text-white">
+                        Dola Re Dandiya
+                    </h1>
+                </li>
+                <li className="flex items-center">
+                    <a href="/" className="text-white hover:text-blue-200 font-semibold">Home</a>
+                </li>
+                <li className="flex items-center">
+                    <a href="/about" className="text-white hover:text-blue-200 font-semibold">About</a>
+                </li>
+                <li className="flex items-center">
+                    <a href="/currSponsors" className="text-white hover:text-blue-200 font-semibold">Current Sponsors</a>
+                </li>
+                <li className="flex items-center">
+                    <a href="/potenSponsors" className="text-white hover:text-blue-200 font-semibold">Potential Sponsors</a>
+                </li>
+                <li className="flex items-center">
+                    <a href="/contact" className="text-white hover:text-blue-200 font-semibold">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
