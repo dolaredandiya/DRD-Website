c444fb33c7a598f3f872127a7d0fb8c15b526b35
Pranav Mucharla <mucharlapranav@gmail.com>
2025-06-30 21:28:58 -0400

    Change background to automatically center for smaller screens

--- drd-website/src/app/page.tsx
+++ drd-website/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[url('/boardBackground.png')] bg-fixed bg-cover bg-no-repeat">
+    <div className="min-h-screen bg-[url('/boardBackground.png')] bg-fixed bg-cover bg-no-repeat bg-center">
       <div className="flex min-h-screen items-center justify-center">
         <div className="bg-white rounded-2xl px-8 pt-4 pb-6 shadow-lg bg-opacity-90">
           <div className="text-5xl md:text-7xl text-harvest-500 text-center drop-shadow-lg">
